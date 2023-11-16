d0ccdb9ef976768436766ced4bf1f741c6ed95d8
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-11-16 21:26:39 +0300

    Add project files.

---
+++ ElearningProject/DAL/Entities/Instructor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElearningProject.DAL.Entities
+{
+    public class Instructor
+    {
+        public int InstructorID { get; set; }
+        public string Name { get; set; }
+        [StringLength(30)]
+        public string Surname { get; set; }
+
+    }
+}
