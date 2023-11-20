6375a0eb439fa6a0bd74b743226ca452cda83e78
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-11-20 19:39:41 +0300

    StudentController eklendi

---
+++ ElearningProject/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using ElearningProject.DAL.Context;
+using ElearningProject.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElearningProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        ELearningContext context = new ELearningContext();
+        public ActionResult Index()
+        {
+            var values = context.Students.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddStudent()
+        {
+            return View();
+        }
+        public ActionResult AddStudent(Student student)
+        {
+            context.Students.Add(student);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
