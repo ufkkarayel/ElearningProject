84e393ef1b12455264c47f2704849a1855c96e56
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-11-21 20:04:45 +0300

    update ve delete işlemleri eklendi

--- ElearningProject/Controllers/StudentController.cs
+++ ElearningProject/Controllers/StudentController.cs
@@ -28,5 +28,29 @@ namespace ElearningProject.Controllers
             context.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult DeleteStudent(int id)
+        {
+            var value = context.Students.Find(id);
+            context.Students.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateStudent(int id)
+        {
+            var value = context.Students.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateStudent(Student student)
+        {
+            var value = context.Students.Find(student.StudentID);
+            value.Name = student.Name;
+            value.Surname = student.Surname;
+            value.Email = student.Email;
+            value.Password = student.Password;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
