084c3c810b999b5ff21b1ca6b4b9d2a80d67d12f
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-11-25 23:31:59 +0300

    .

--- ElearningProject/Controllers/InstructorAnalysisController.cs
+++ ElearningProject/Controllers/InstructorAnalysisController.cs
@@ -18,19 +18,21 @@ namespace ElearningProject.Controllers
         public PartialViewResult InstructorPanelPartial()
         {
             int id = 1;
-            var values = context.Instructors.Where(x => x.InstructorID == id).ToList(); ;
+            var values = context.Instructors.Where(x => x.InstructorID == id).ToList(); 
+            var v1 = context.Instructors.Where(x => x.Name == "Fatih" && x.Surname == "Çakıroğlu").Select(y => y.InstructorID).FirstOrDefault();
+            var v2 = context.Courses.Where(x => x.InstructorID == v1).Select(y => y.CourseID).ToList();
+            ViewBag.courseCount = context.Courses.Where(x => x.InstructorID == 1).Count();
+            ViewBag.commentCount = context.Comments.Where(x => v2.Contains(x.CourseID)).Count();
+            
             return PartialView(values);
         }
         public PartialViewResult CommentPartial()
         {
             //eğitmen ID getirmek için
             var v1 = context.Instructors.Where(x => x.Name == "Fatih" && x.Surname == "Çakıroğlu").Select(y => y.InstructorID).FirstOrDefault();
-
-            //v2 değişkeninin için eğitmen ID v1 olan değeri atamak için
             var v2 = context.Courses.Where(x => x.InstructorID == v1).Select(y => y.CourseID).ToList();
-
             var v3 = context.Comments.Where(x => v2.Contains(x.CourseID)).ToList();
-
+                      
             return PartialView(v3);
         }
         public PartialViewResult CourseListByInstructor()
